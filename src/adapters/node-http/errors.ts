13e9dbaebbde700a2cc4443b50d299832735cae4
Henry Cho <hscho817@gmail.com>
2024-05-07 08:35:24 +0000

    feat: update

--- src/adapters/node-http/errors.ts
+++ src/adapters/node-http/errors.ts
@@ -18,7 +18,7 @@ export const TRPC_ERROR_CODE_HTTP_STATUS: Record<TRPCError['code'], number> = {
 };
 
 export function getErrorFromUnknown(cause: unknown): TRPCError {
-  if (cause instanceof Error && cause.name === 'TRPCError') {
+  if (cause instanceof Error && cause instanceof TRPCError) {
     return cause as TRPCError;
   }
 
